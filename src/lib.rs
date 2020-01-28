440d25d5e39b0e28c0afeb48b67ebbbe86de64c8
jmcph4 <jmcph4@users.noreply.github.com>
2020-01-28 12:54:09 +1000

    Add constructor, `render` method for `Universe` type
    This commit adds both a constructor and a rendering method for the
    `Universe` type.

--- src/lib.rs
+++ src/lib.rs
@@ -76,6 +76,31 @@ impl Universe {
 
         self.cells = next;
     }
+
+pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
 }
 
 use std::fmt;
