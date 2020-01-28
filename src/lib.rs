371224c420e4cdbd82a513e3990930904fe86da8
jmcph4 <jmcph4@users.noreply.github.com>
2020-01-28 12:33:25 +1000

    Add `live_neighbour_count` method to `Universe` type
    This commit adds the `live_neighbour_count` method to the `Universe`
    type, which returns the number of living cells in the neighbourhood of
    the provided co-ordinates.

--- src/lib.rs
+++ src/lib.rs
@@ -27,5 +27,24 @@ impl Universe {
     fn get_index(&self, row: u32, column: u32) -> usize {
         (row * self.width + column) as usize
     }
+
+    fn live_neighbour_count(&self, row: u32, column: u32) -> u8 {
+        let mut count: u8 = 0;
+
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbour_row: usize = (row + delta_row) % self.height;
+                let neighbour_col: usize = (column + delta_col) % self.width;
+                let idx: usize = self.get_index(neighbour_row, neighbour_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+
+        count
+    }
 }
 
