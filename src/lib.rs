2179d6db56ce176f5d7f269550881e0ae31747f6
jmcph4 <jmcph4@users.noreply.github.com>
2020-01-28 12:39:01 +1000

    Add `tick` method to `Universe` type
    This commit adds the `tick` method to the `Universe` type. This method
    drives the state changes in the simulation.

--- src/lib.rs
+++ src/lib.rs
@@ -48,3 +48,30 @@ impl Universe {
     }
 }
 
+#[wasm_bindgen]
+impl Universe {
+    pub fn tick(&mut self) {
+        let mut next: Cell = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx: usize = self.get_index(row, col);
+                let cell: Cell = self.cells[idx];
+                let live_neighbours: u8 = self.live_neighbour_count(row, col);
+
+                let next_cell: Cell = match (cell, live_neighbours) {
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    (Cell::Dead, 3) => Cell::Alive,
+                    (otherwise, _) => otherwise
+                };
+
+                next[idx] = next_cell;
+            }
+        }
+
+        self.cells = next;
+    }
+}
+
