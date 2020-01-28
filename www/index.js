ebdd15c2934eda75e2c61e674f84a22a6407579b
jmcph4 <jmcph4@users.noreply.github.com>
2020-01-28 12:58:29 +1000

    Add frontend rendering code
    This commit adds the appropriate HTML markup and Javascript code to the
    frontend to enable rendering the simulation to the screen via the
    client's browser.

--- www/index.js
+++ www/index.js
@@ -1,3 +1,14 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet("Jack");
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+
+  requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
+
